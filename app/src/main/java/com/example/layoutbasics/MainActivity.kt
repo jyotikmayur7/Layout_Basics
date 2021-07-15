edc88a00d1005e960711dfa314880637c942fe4f
Jyotik Mayur <heyjyotik@gmail.com>
2021-07-15 18:51:30 +0100

    created setupQuestions() to add questions in the questions list

--- app/src/main/java/com/example/layoutbasics/MainActivity.kt
+++ app/src/main/java/com/example/layoutbasics/MainActivity.kt
@@ -4,9 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private var questions: MutableList<Question> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        setContentView(R.layout.mixed)
+        //setContentView(R.layout.mixed)
+        setContentView(R.layout.dynamic_layout)
+
+        setupQuestions()
+
     }
+
+    private fun setupQuestions(){
+        questions.add(Question(1, QuestionType.Text, "What is the capital of Nagaland?", null, listOf("kohima")))
+        questions.add(Question(2, QuestionType.Radio,"Which is the largest(area) state in India?",
+            listOf("Bihar", "Madhya Pradesh", "Uttar Pradesh", "Rajasthan"), listOf("Rajasthan")))
+        questions.add(Question(3, QuestionType.Checkbox, "Which of these are state capitals?",
+            listOf("Guwahati","Chennai","Vanarasi","Dispur"), listOf("Chennai","Dispur")))
+    }
+
 }
